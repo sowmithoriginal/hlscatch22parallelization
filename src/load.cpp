285dc176fefd22fc41bd93103878145904871d6c
sowmith <130018680+sowmithoriginal@users.noreply.github.com>
2024-05-21 15:48:22 -0700

    Update load.cpp

--- src/load.cpp
+++ src/load.cpp
@@ -1,1 +1,14 @@
+#include "constants.h"
 
+extern "C" {
+    void load(data_t* input, hls::stream<data_t> &output) {
+        #pragma HLS INTERFACE m_axi port = input bundle = gmem0
+        #pragma HLS INTERFACE axis port = output
+
+        #pragma HLS DATAFLOW
+        
+        static int counter = 0; 
+        output << input[counter];
+        counter+=1; 
+    }
+}
