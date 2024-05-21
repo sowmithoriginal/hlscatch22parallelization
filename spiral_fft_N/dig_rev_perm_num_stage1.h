31b3789d7f8378a4aace4cc36f2796f2588f3908
sowmith <130018680+sowmithoriginal@users.noreply.github.com>
2024-05-21 16:01:12 -0700

    Create dig_rev_perm_num_stage1.h

---
+++ spiral_fft_N/dig_rev_perm_num_stage1.h
@@ -0,0 +1,120 @@
+#if N1 == 16 && SW1 == 2 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 1
+#endif
+#if N1 == 16 && SW1 == 4 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 16 && SW1 == 8 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 1
+#endif
+#if N1 == 32 && SW1 == 2 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 1
+#endif
+#if N1 == 32 && SW1 == 4 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 32 && SW1 == 8 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 64 && SW1 == 2 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 1
+#endif
+#if N1 == 64 && SW1 == 4 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 64 && SW1 == 8 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 3
+#endif
+#if N1 == 128 && SW1 == 2 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 1
+#endif
+#if N1 == 128 && SW1 == 4 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 128 && SW1 == 8 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 3
+#endif
+#if N1 == 256 && SW1 == 2 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 1
+#endif
+#if N1 == 256 && SW1 == 4 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 256 && SW1 == 8 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 3
+#endif
+#if N1 == 512 && SW1 == 2 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 1
+#endif
+#if N1 == 512 && SW1 == 4 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 512 && SW1 == 8 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 3
+#endif
+#if N1 == 1024 && SW1 == 2 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 1
+#endif
+#if N1 == 1024 && SW1 == 4 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 1024 && SW1 == 8 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 3
+#endif
+#if N1 == 2048 && SW1 == 2 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 1
+#endif
+#if N1 == 2048 && SW1 == 4 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 2048 && SW1 == 8 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 3
+#endif
+#if N1 == 4096 && SW1 == 2 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 1
+#endif
+#if N1 == 4096 && SW1 == 4 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 4096 && SW1 == 8 && R1 == 2
+#define DIGIT_REV_NUM_STAGE 3
+#endif
+#if N1 == 16 && SW1 == 4 && R1 == 4
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 16 && SW1 == 8 && R1 == 4
+#define DIGIT_REV_NUM_STAGE 1
+#endif
+#if N1 == 64 && SW1 == 4 && R1 == 4
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 64 && SW1 == 8 && R1 == 4
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 256 && SW1 == 4 && R1 == 4
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 256 && SW1 == 8 && R1 == 4
+#define DIGIT_REV_NUM_STAGE 3
+#endif
+#if N1 == 1024 && SW1 == 4 && R1 == 4
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 1024 && SW1 == 8 && R1 == 4
+#define DIGIT_REV_NUM_STAGE 3
+#endif
+#if N1 == 4096 && SW1 == 4 && R1 == 4
+#define DIGIT_REV_NUM_STAGE 2
+#endif
+#if N1 == 4096 && SW1 == 8 && R1 == 4
+#define DIGIT_REV_NUM_STAGE 3
+#endif
+#if N1 == 64 && SW1 == 8 && R1 == 8
+#define DIGIT_REV_NUM_STAGE 3
+#endif
+#if N1 == 512 && SW1 == 8 && R1 == 8
+#define DIGIT_REV_NUM_STAGE 3
+#endif
+#if N1 == 4096 && SW1 == 8 && R1 == 8
+#define DIGIT_REV_NUM_STAGE 3
+#endif
